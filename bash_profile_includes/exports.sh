eb9616a015248ee6b55d3010fadcfdf135b3ff68
Matt Ordonez <mattorus@gmail.com>
2015-05-23 09:53:12 -0700

    Add PATH for local node_modules

--- bash_profile_includes/exports.sh
+++ bash_profile_includes/exports.sh
@@ -14,3 +14,4 @@ export PATH=/usr/local/share/npm/bin/:$PATH
 export PATH=/usr/local/lib/jsctags:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=~/bin:$PATH
+export PATH=./node_modules/.bin:$PATH
