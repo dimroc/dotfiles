f249e0e6a6605a57086850ce9fcc4adabc96d216
Dimitri Roche <dimroc@gmail.com>
2017-04-11 18:43:14 -0400

    Remove PYTHONPATH for python3 and add ./bin

--- bash_profile_includes/exports.sh
+++ bash_profile_includes/exports.sh
@@ -8,8 +8,11 @@ export LSCOLORS=gxfxcxdxbxegedabagacad # Dark lscolor scheme
 export HISTCONTROL=ignoredups
 export EDITOR=vim
 export VISUAL=mvim
+# The following were removed because it causes python3 and pip3 to crash.
+# Those are needed for tensorflow
+# https://github.com/Homebrew/homebrew-core/issues/9229
 #export PYTHONPATH=/usr/local/bin/python
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+#export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
 export NVM_DIR="$HOME/.nvm"
 export FZF_DEFAULT_COMMAND='ag -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMAND"
@@ -21,3 +24,4 @@ export PATH=/usr/local/bin:$PATH
 export PATH=~/bin:$PATH
 export PATH=~/.composer/vendor/bin:$PATH
 export PATH=./node_modules/.bin:$PATH
+export PATH=./bin:$PATH
