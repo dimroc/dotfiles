004c95213a540ecb08f176dc19e0afdc7b1521fc
Dimitri Roche <dimroc@gmail.com>
2017-04-16 08:54:06 -0400

    Add iterm2, gcloud integration. consolidate bash completion

--- bash_profile_includes/externals.sh
+++ bash_profile_includes/externals.sh
@@ -14,3 +14,17 @@ eval "$(direnv hook bash)"
 
 #fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+#iterm2
+test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+#gcloud
+pathcompletion='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
+[ -f $pathcompletion ] && source "$pathcompletion"
+bashcompletion='/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
+[ -f $bashcompletion ] && source "$bashcompletion"
+
+# bash git completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  . `brew --prefix`/etc/bash_completion
+fi
