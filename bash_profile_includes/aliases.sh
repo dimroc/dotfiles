e0a07f0c82b3f369a5f8d885a64ecfc03c5e39e9
Dimitri Roche <dimroc@gmail.com>
2018-01-03 10:19:31 -0500

    Add vimplain

--- bash_profile_includes/aliases.sh
+++ bash_profile_includes/aliases.sh
@@ -9,6 +9,7 @@ alias rmquarantine='xattr -d com.apple.quarantine'
 alias stop='kill -2'
 alias shutupvim="rm ~/.vim/.tmp/*.sw*"
 alias mvimplain="mvim -u NONE"
+alias vimplain="vim -u NONE"
 
 alias gemdoc='gem environment gemdir'/doc
 alias untar="tar -xvf"
