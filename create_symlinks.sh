521407092a28a9997aaf06d1160c197c74c3b541
Dimitri Roche <dimroc@gmail.com>
2018-08-08 11:23:22 -0400

    Add grc to dotfiles

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=(zshrc ackrc bash_profile bash_profile_includes gitconfig gitignore gituserconfig inputrc irbrc rdebugrc gemrc agignore direnvrc tmux.conf)
+dotfiles=(zshrc ackrc bash_profile bash_profile_includes gitconfig gitignore gituserconfig inputrc irbrc rdebugrc gemrc agignore direnvrc tmux.conf grc)
 source=$1
 
 if [ -z $source ]; then
