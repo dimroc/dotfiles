3cd38439dd97f590cc9fed1ec6d93f25634658f0
Dimitri Roche <dimroc@gmail.com>
2018-12-24 09:43:43 -0500

    Remove the inclusion of the travis gem

--- bash_profile_includes/externals.sh
+++ bash_profile_includes/externals.sh
@@ -3,9 +3,6 @@
 # rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 
-# added by travis gem
-[ -f "$HOME/.travis/travis.sh" ] && . "$HOME/.travis/travis.sh"
-
 # Added by direnv
 eval "$(direnv hook bash)"
 
